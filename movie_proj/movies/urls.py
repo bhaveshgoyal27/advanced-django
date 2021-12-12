f4d6f1e8566dc5d4626fd0d5081f9bbbfbe2c216
bhaveshgoyal27 <27goyalbhavesh@gmail.com>
2021-12-12 17:42:02 +0530

    forms and model forms

---
+++ movie_proj/movies/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+#20mins
--- movie_proj/movies/views.py
+++ movie_proj/movies/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from .models import Film, Commercial
+from .forms import MovieSelectForm, FilmModelForm, CommercialModelForm
 # Create your views here.
+
+class MovieSelectForm(FormView):
+	form_class = MovieSelectForm
+	template_name = "movies/main.html"
+	success_url = reverse_lazy("home")
+
+	def post(self, *args, **kwargs):
+		self.request.session["movie"] = sef.request.POST.get("movie")
+		return super().post(*args, **kwargs)
+
+class AddMovieForm(FormView):
+	template_name = "movies/add.html"
+	success_url = reverse_lazy("home")
+
+	def get_form_class(self, *args, **kwargs):
+		pass
+
